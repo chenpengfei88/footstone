068e9c3a74df1e7a8bacd3e864c43bcde42941a4
陈鹏飞 <pengfei.chen@weimob.com>
2017-05-22 14:42:14 +0800

    first commit

---
+++ app/src/main/java/com/cpf/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cpf.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.footstone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
